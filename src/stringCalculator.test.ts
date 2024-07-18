eb9a7fe26081998cc38952ac7f15d1a8b54bb0a8
Yash <Yashkadam0530@gmail.com>
2024-07-18 23:17:24 +0530

    feat: handle custom delimiter of length 1

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -26,4 +26,8 @@ describe('String Calculator', () => {
   test('handle new lines between numbers', () =>{
     expect(calculator.add("1\n2,3")).toBe(6);
   });
+
+  test('handle custom delimiter of length 1', () =>{
+    expect(calculator.add("//;\n1;2;3")).toBe(6);
+  });
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,16 @@
 export class stringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
-      const numbersArray = numbers.split(/[\n,]/);
+      let numbersOnlyString = numbers;
+      let delimiter : RegExp = /[\n,]/;
+
+      // Check if custom delimiter exists
+      if(numbers.startsWith("//")){
+        delimiter = new RegExp(numbers[2]);
+        numbersOnlyString = numbers.split("\n",2)[1];
+      }
+      
+      const numbersArray = numbersOnlyString.split(delimiter);
       let sum = 0;
       numbersArray.forEach((n) =>{
         sum += parseInt(n,10);
